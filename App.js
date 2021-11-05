9fbd0d2fc3725ff9b7d86dd8c770f1f85464749c
dvvill <91037458+dvvill@users.noreply.github.com>
2021-11-04 20:23:20 -0400

    Reverted previous changes
    Reverted the changes made in the last/previous commit.  This looks closer to desired activity

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router,  Route, Switch } from "react-router-dom"
-import './App.css';
+import './App.css';	
 import Navbar from './Components/Navbar';
-import Lists from './Components/Lists';
-import Today from './Components/Today';
-import Week from './Components/Week';
 
 
-const App = () => {
-  return (  
-    <div className="App">     
-      <Router>
-      
-          <Navbar />
-         
-          <Route path='/Lists' component={Lists}/>
-          <Route path='/Today' component={Today} />
-          <Route path='/Week' component={Week} />
-        
-      </Router>
-    </div>
-  );
+function App() {
+return (
+<div className="App">
+<Navbar />
+</div>
+);
 }
-
 
 export default App;
--- Navbar.js
+++ Navbar.js
@@ -1,18 +1,42 @@
-import '../App.css';
-import { Link } from "react-router-dom"
+import React, {useState} from 'react'	
+	import '../App.css';
+	import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+	import Lists from './Lists';
+	import Today from './Today';
+	import Week from './Week';
+	//import { NavLink } from 'react-router-dom';
+	
+	
+	const Navbar = () => {
+	    // const [navbar, setNavbar] = useState ();
+	
+        return (
+	    <Router>
+	        <div className='navbar'>
+	            <nav>
+	                <ul className='navlinks'>
+	                    <li className='lists'><Link to='/Lists'>Lists</Link></li>
+	                    <li className='today'><Link to ='/Today'>Today</Link></li>
+	                    <li className='week'><Link tp='/Week'>Week</Link></li>
+	                </ul>
+	            </nav>
 
-
-const Navbar = () => {
-    return (
-            <div className='navbar'> 
-                    <ul className='nav'>
-                        <li><Link to="/Lists">Lists</Link></li>
-                        <li><Link to ='/Today'>Today</Link></li>                     
-                        <li><Link to='/Week'>Week</Link></li>
-                    </ul>                                                
-            </div>     
-    );
-}
-
-
-export default Navbar;
+	        <Switch>
+	            <Route path='/Lists'> 
+	                <Lists />
+	            </Route>
+	            <Route path='/Today'>
+	                <Today />
+	            </Route>
+	            <Route path='/Week'>
+	                <Week />
+	            </Route>
+	        </Switch>
+	
+	        </div>
+	    </Router>
+	    );
+	}
+	
+	
+	export default Navbar;
