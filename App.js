299d973a4d40cc0d36c8d925b2050c836d3e7fba
dvvill <91037458+dvvill@users.noreply.github.com>
2021-11-05 00:28:25 -0400

    edited App.js moved LINKS
    Moved the Navbar, Lists, Today and Week components into App.js to get links to redirect to their specific paths '/'

--- App.js
+++ App.js
@@ -1,13 +1,63 @@
 import './App.css';	
-import Navbar from './Components/Navbar';
+import React from 'react'
+import {  Switch, Route, Link } from "react-router-dom"
 
 
 function App() {
 return (
 <div className="App">
-<Navbar />
+  <header>
+    <h1>Welcome</h1>
+  </header>
+
+  <Route>
+      <Switch>
+	            <Route path='/Lists' element={<Lists />} /> 
+	            <Route  path='/Today' element={<Today />}/>  
+	            <Route path='/Week' element={<Week />} />	               
+	        </Switch>
+    </Route>
 </div>
 );
 }
+
+function Lists() {
+  return (
+      <>
+      <div className="list">
+          <h1>Lists Page</h1>
+      </div> 
+      <nav>
+      <Link to='/Lists'>Lists</Link>
+      </nav>
+      </>
+  );
+};
+
+function Today() {
+  return (
+      <>
+      <div className="today">
+          <h1>Today</h1>
+      </div> 
+      <nav>
+      <Link to ='/Today'>Today</Link>
+      </nav>
+      </>
+  );
+};
+
+function Week() {
+  return (
+      <>
+      <div className="week">
+          <h1>Week Page</h1>
+      </div> 
+      <nav>
+      <Link to='/Week'>Week</Link>
+      </nav>
+      </>     
+  );
+};
 
 export default App;
